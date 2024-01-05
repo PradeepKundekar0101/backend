a25b19a4e5475eac184a537c070b9374b3f2777d
Kishor Balgi <75678927+KishorBalgi@users.noreply.github.com>
2024-01-05 21:49:55 +0530

    Bug/helpdesk return type (#36)
    * Created s3 client and putObjectUrl
    
    * Added getObjectUrl
    
    * update and delete objects
    
    * file upload handling
    
    * modified src/services/analytics.ts
    
    * updated videos updation logic
    
    * refactor: some improvements
    
    * fix: Merge Conflict
    
    * fix: return type
    
    ---------
    
    Co-authored-by: Pradeep Kundekar <pradeepkundekar1010@gmail.com>

--- src/services/helpdesk.ts
+++ src/services/helpdesk.ts
@@ -5,51 +5,64 @@ import HelpDesk, { IHelpDesk } from "../models/helpdesk";
 type IUpdatedHelpDesk = Document<unknown, {}, IHelpDesk>;
 
 class HelpDeskService {
-  async createHelpDesk(helpdeskData: Partial<IHelpDesk>, helpdeskImage: Express.Multer.File): Promise<IHelpDesk> {
-    const imageName = await putObjectURL(helpdeskImage,"helpdesk");
-    const helpdesk = await HelpDesk.create({title:helpdeskData.title,url:helpdeskData.url,image_name:imageName});
+  async createHelpDesk(
+    helpdeskData: Partial<IHelpDesk>,
+    helpdeskImage: Express.Multer.File
+  ): Promise<IHelpDesk> {
+    const imageName = await putObjectURL(helpdeskImage, "helpdesk");
+    const helpdesk = await HelpDesk.create({
+      title: helpdeskData.title,
+      url: helpdeskData.url,
+      image_name: imageName,
+    });
     return helpdesk;
   }
 
   async getAllHelpDesk(): Promise<IHelpDesk[]> {
     const helpdesks = await HelpDesk.find();
-    for(const helpdesk of helpdesks)
-    {
-        const image_name = helpdesk.image_name;
-        helpdesk.image_url = await getObjectURL(image_name);
+    for (const helpdesk of helpdesks) {
+      const image_name = helpdesk.image_name;
+      helpdesk.image_url = await getObjectURL(image_name);
     }
     return helpdesks;
   }
 
   async getHelpDeskById(helpdeskId: string): Promise<IHelpDesk | null> {
     const helpdesk = await HelpDesk.findById(helpdeskId);
-    if(!helpdesk) return null;
+    if (!helpdesk) return null;
     const image_name = helpdesk?.image_name;
     helpdesk.image_url = await getObjectURL(image_name);
-    
+
     return helpdesk;
   }
 
-  async updateHelpDesk( helpdeskId: string, helpdeskData: IHelpDesk ,helpdeskImage:Express.Multer.File | undefined): Promise<IUpdatedHelpDesk | null> {
+  async updateHelpDesk(
+    helpdeskId: string,
+    helpdeskData: IHelpDesk,
+    helpdeskImage: Express.Multer.File | undefined
+  ): Promise<IUpdatedHelpDesk | null> {
     let imageName: string | undefined;
     if (helpdeskImage) {
-      imageName = await putObjectURL(helpdeskImage,"helpdesk");
+      imageName = await putObjectURL(helpdeskImage, "helpdesk");
     }
     if (imageName) {
-      helpdeskData.image_name = imageName; 
+      helpdeskData.image_name = imageName;
     }
-    const helpdesk = await HelpDesk.findByIdAndUpdate(helpdeskId,helpdeskData,{ new: true }
+    const helpdesk = await HelpDesk.findByIdAndUpdate(
+      helpdeskId,
+      helpdeskData,
+      { new: true }
     );
-    return helpdesk
+    return helpdesk;
   }
 
-  async deleteHelpDesk(helpdeskId: string): Promise<IHelpDesk | null> {
-     //Delete the Image from s3
-     const helpdeskFound = await HelpDesk.findById(helpdeskId);
-     if(!helpdeskFound) return null;
-     await deleteObject(helpdeskFound.image_name);
-     const helpdesk = await HelpDesk.findByIdAndDelete(helpdeskId);
-     return helpdesk;
+  async deleteHelpDesk(helpdeskId: string): Promise<IUpdatedHelpDesk | null> {
+    //Delete the Image from s3
+    const helpdeskFound = await HelpDesk.findById(helpdeskId);
+    if (!helpdeskFound) return null;
+    await deleteObject(helpdeskFound.image_name);
+    const helpdesk = await HelpDesk.findByIdAndDelete(helpdeskId);
+    return helpdesk;
   }
 }
 
